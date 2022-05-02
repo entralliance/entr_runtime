f15c99d6d228fa44392e5f824a19c8c0840159a0
Jordan Perr-Sauer <jordan.perr-sauer@nrel.gov>
2022-05-02 17:57:57 -0600

    manually specifying metastore db location:

--- docker/start_entr_services.sh
+++ docker/start_entr_services.sh
@@ -4,7 +4,10 @@
 runuser -l postgres -c '/usr/lib/postgresql/9.3/bin/postgres -D /var/lib/postgresql/9.3/main -c config_file=/etc/postgresql/9.3/main/postgresql.conf' &
 
 # Spark
-/usr/local/spark/bin/spark-submit --conf spark.sql.warehouse.dir=/home/jovyan/warehouse --class org.apache.spark.sql.hive.thriftserver.HiveThriftServer2 &
+/usr/local/spark/bin/spark-submit\
+    --conf spark.sql.warehouse.dir=/home/jovyan/warehouse\
+    --conf spark.hadoop.javax.jdo.option.ConnectionURL=jdbc:derby:\;databaseName=/home/jovyan/warehouse/metastore_db\;create=true\
+    --class org.apache.spark.sql.hive.thriftserver.HiveThriftServer2 &
 
 # Jupyter
 /usr/local/bin/start-notebook.sh &

2cf48d3b4f05d0bcb97ab7bac6b7a30e35fd0abe
Jordan Perr-Sauer <jordan.perr-sauer@nrel.gov>
2022-05-02 13:25:14 -0600

    fixed issue where services were stopping after 60 seconds. set  warehouse directory in /home/jovyan/warehouse

--- docker/start_entr_services.sh
+++ docker/start_entr_services.sh
@@ -4,7 +4,7 @@
 runuser -l postgres -c '/usr/lib/postgresql/9.3/bin/postgres -D /var/lib/postgresql/9.3/main -c config_file=/etc/postgresql/9.3/main/postgresql.conf' &
 
 # Spark
-/usr/local/spark/bin/spark-submit --class org.apache.spark.sql.hive.thriftserver.HiveThriftServer2 &
+/usr/local/spark/bin/spark-submit --conf spark.sql.warehouse.dir=/home/jovyan/warehouse --class org.apache.spark.sql.hive.thriftserver.HiveThriftServer2 &
 
 # Jupyter
 /usr/local/bin/start-notebook.sh &
--- docker/startup_wrapper_script.sh
+++ docker/startup_wrapper_script.sh
@@ -9,14 +9,5 @@ start_entr_services.sh &
 # Otherwise it loops forever, waking up every 60 seconds
 
 while sleep 60; do
-  ps aux |grep my_first_process |grep -q -v grep
-  PROCESS_1_STATUS=$?
-  ps aux |grep my_second_process |grep -q -v grep
-  PROCESS_2_STATUS=$?
-  # If the greps above find anything, they exit with 0 status
-  # If they are not both 0, then something is wrong
-  if [ $PROCESS_1_STATUS -ne 0 -o $PROCESS_2_STATUS -ne 0 ]; then
-    echo "One of the processes has already exited."
-    exit 1
-  fi
+  echo "ENTR Heartbeat"
 done

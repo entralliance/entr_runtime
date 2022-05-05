621551b1f79986583651b4253560c44acf47520f
Jordan Perr-Sauer <jordan.perr-sauer@nrel.gov>
2022-05-05 16:22:18 -0600

    Update start_entr_services.sh

--- docker/start_entr_services.sh
+++ docker/start_entr_services.sh
@@ -5,6 +5,7 @@ runuser -l postgres -c '/usr/lib/postgresql/9.3/bin/postgres -D /var/lib/postgre
 
 # Spark
 /usr/local/spark/bin/spark-submit\
+    --conf spark.hive.server2.thrift.bind.host=localhost\
     --conf spark.sql.warehouse.dir=/home/jovyan/warehouse\
     --conf spark.hadoop.javax.jdo.option.ConnectionURL=jdbc:derby:\;databaseName=/home/jovyan/warehouse/metastore_db\;create=true\
     --class org.apache.spark.sql.hive.thriftserver.HiveThriftServer2 &
